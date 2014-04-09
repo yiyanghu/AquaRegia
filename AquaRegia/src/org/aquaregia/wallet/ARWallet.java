22377bbffcc512379b3a77c136c06036eb2ae120
Steve Halm <thereal1024@gmail.com>
2014-04-09 16:28:58 -0500

    fix javadoc

--- AquaRegia/src/org/aquaregia/wallet/ARWallet.java
+++ AquaRegia/src/org/aquaregia/wallet/ARWallet.java
@@ -1,16 +1,26 @@
 package org.aquaregia.wallet;
 
+import java.io.File;
+
 import com.google.bitcoin.core.NetworkParameters;
 import com.google.bitcoin.params.MainNetParams;
-import com.google.bitcoin.params.RegTestParams;
+import com.google.bitcoin.kits.WalletAppKit;
 
 
 public class ARWallet {
-
+	public static final String WALLET_DEFAULT = "default";
+	
 	// make the wallet work on main bitcoin network
 	NetworkParameters params = MainNetParams.get();
+	WalletAppKit bitcoin;
 	
 	public ARWallet() {
-		
+		this(WALLET_DEFAULT);
+	}
+	
+	public ARWallet(String walletName) {
+		assert(walletName != null);
+		bitcoin = new WalletAppKit(params, new File("."),  walletName);
+		//
 	}
 }
