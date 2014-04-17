2185d642133484889aa9d4b64b18df0c87b60933
Steve Halm <thereal1024@gmail.com>
2014-04-17 14:42:10 -0500

    clean up Deterministic class p2

--- AquaRegia/src/org/aquaregia/wallet/Deterministic.java
+++ AquaRegia/src/org/aquaregia/wallet/Deterministic.java
@@ -14,20 +14,37 @@ public class Deterministic {
 	
 	public static final BigInteger N = new BigInteger("115792089237316195423570985008687907852837564279074904382605163141518161494337");
 	
-	public static byte[] doubleSHA256(byte[] input) {
-		for(int i=0;i<2;i++){
+	public static byte[] getMasterPrivateKey(byte[] seed) {
+		byte[] original = seed;	
+		// hash seed with SHA256 100,000 times
+		for(int i = 0; i < 100000; i++) {
 			MessageDigest md = null;
 			try {
 				md = MessageDigest.getInstance("SHA-256");
 			} catch (NoSuchAlgorithmException e) {
 				return null;
 			}
-			md.update(input);
-			input = md.digest();
-		}
-		return input;
-	}
 	
+			md.update(byteArrayConcat(seed, original));
+			seed = md.digest();
+		}
+		return seed;
+	}
+
+	/**
+	 * This function generates master public key from a wallet seed
+	 * @param seed - a wallet seed
+	 * @return master public key
+	 */
+	public static byte[] getMasterPublicKey (byte[] seed) {
+		byte[] masterPrivateKey = getMasterPrivateKey(seed);
+		ECKey masterKeyPair = new ECKey(masterPrivateKey,null);	
+		byte [] result = masterKeyPair.getPubKey(); 
+		result = Arrays.copyOfRange(result, 1, result.length);
+		return result;
+		
+	}
+
 	public static byte[] getPrivateKey(byte[] masterPrivateKey,int n, int isChangeAddr) {
 		ECKey masterKeyPair = new ECKey(masterPrivateKey,null);	
 		byte[] masterPublicKey = masterKeyPair.getPubKey();
@@ -49,23 +66,6 @@ public class Deterministic {
 		return Utils.bigIntegerToBytes(sum, 32);
 	}
 
-	public static byte[] getMasterPrivateKey(byte[] seed) {
-		byte[] original = seed;	
-		// hash seed with SHA256 100,000 times
-		for(int i = 0; i < 100000; i++) {
-			MessageDigest md = null;
-			try {
-				md = MessageDigest.getInstance("SHA-256");
-			} catch (NoSuchAlgorithmException e) {
-				return null;
-			}
-	
-			md.update(byteArrayConcat(seed, original));
-			seed = md.digest();
-		}
-		return seed;
-	}
-	
 	public static byte[] byteArrayConcat(byte[] first, byte[] second) {
 		byte[] result = new byte[first.length + second.length];
 		System.arraycopy(first, 0, result, 0, first.length);
@@ -77,18 +77,18 @@ public class Deterministic {
 		return DatatypeConverter.printHexBinary(bytes).toLowerCase();
 	}
 	
-	/**
-	 * This function generates master public key from a wallet seed
-	 * @param seed - a wallet seed
-	 * @return master public key
-	 */
-	public static byte[] getMasterPublicKey (byte[] seed) {
-		byte[] masterPrivateKey = getMasterPrivateKey(seed);
-		ECKey masterKeyPair = new ECKey(masterPrivateKey,null);	
-		byte [] result = masterKeyPair.getPubKey(); 
-		result = Arrays.copyOfRange(result, 1, result.length);
-		return result;
-		
+	public static byte[] doubleSHA256(byte[] input) {
+		for(int i=0;i<2;i++){
+			MessageDigest md = null;
+			try {
+				md = MessageDigest.getInstance("SHA-256");
+			} catch (NoSuchAlgorithmException e) {
+				return null;
+			}
+			md.update(input);
+			input = md.digest();
+		}
+		return input;
 	}
 	
 	
