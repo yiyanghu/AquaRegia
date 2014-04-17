163371c12e37aa5afccdfd744423d2617f0c5043
Yiyang Hu <yiyangh@gmail.com>
2014-04-17 02:33:03 -0500

    Add getPrivateKey and test case (failed so far)

--- AquaRegia/src/org/aquaregia/wallet/Deterministic.java
+++ AquaRegia/src/org/aquaregia/wallet/Deterministic.java
@@ -1,12 +1,49 @@
 package org.aquaregia.wallet;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
 import javax.xml.bind.*;
+
 import com.google.bitcoin.core.ECKey;
 
 public class Deterministic {
+	
+	public static final BigInteger N = new BigInteger("115792089237316195423570985008687907852837564279074904382605163141518161494337");
+	
+	public static byte[] bin_dbl_sha256(byte[] input) {
+		for(int i=0;i<2;i++){
+			MessageDigest md = null;
+			try {
+				md = MessageDigest.getInstance("SHA-256");
+			} catch (NoSuchAlgorithmException e) {
+				return null;
+			}
+			md.update(input);
+			input = md.digest();
+		}
+		return input;
+	}
+	
+	public static byte[] getPrivateKey(byte[] masterPrivateKey,int n) {
+		ECKey masterKey = new ECKey(masterPrivateKey,null);	
+		byte[] masterPublicKey = masterKey.getPubKey(); 
+		String indexCode = Integer.toString(n)+":0:"+bytesToHex(masterPublicKey);
+		byte[] offset = bin_dbl_sha256(indexCode.getBytes());
+		return addPrivateKeys(masterPrivateKey,offset);
+		
+	}
+	
+	public static byte[] addPrivateKeys(byte[] key1, byte[] key2) {
+		BigInteger key1Int = new BigInteger(key1);
+		BigInteger key2Int = new BigInteger(key2);
+		BigInteger sum = key1Int.add(key2Int);
+		sum = sum.mod(N);
+		return sum.toByteArray();
+	}
 
 	public static byte[] bin_slowsha(byte[] input) {
 		byte[] result= null;
@@ -43,7 +80,9 @@ public class Deterministic {
 		
 		byte[] masterPrivateKey= bin_slowsha(seed);
 		ECKey masterKey = new ECKey(masterPrivateKey,null);	
-		return masterKey.getPubKey(); 
+		byte [] result = masterKey.getPubKey(); 
+		result = Arrays.copyOfRange(result, 1, result.length);
+		return result;
 		
 	}
 	
--- AquaRegia/test/org/aquaregia/wallet/TestDeterministic.java
+++ AquaRegia/test/org/aquaregia/wallet/TestDeterministic.java
@@ -24,9 +24,28 @@ public class TestDeterministic {
 	@Test
 	public void testGetMasterPublicKey() {
 		String test1 = "484ccb566edb66c65dd0fd2e4d90ef65";
-		String expected1 = "04484e42865b8e9a6ea8262fd1cde666b557393258ed598d842e563ad9e5e6c70a97e387eefdef123c1b8b4eb21fe210c6216ad7cc1e4186fbbba70f0e2c062c25";
+		String expected1 = "484e42865b8e9a6ea8262fd1cde666b557393258ed598d842e563ad9e5e6c70a97e387eefdef123c1b8b4eb21fe210c6216ad7cc1e4186fbbba70f0e2c062c25";
 		getMasterPublicKeyHelper(expected1,test1);	
 	
+	}
+	
+	@Test
+	public void testGetPrivateKey() {
+		String seed = "0123456789abcdef0123456789abcdef";
+		String expected1 = "e2a8eb8c2c246060f5fba60243d926855c088d9ab2e5a86b0c7b658c6be2ca9d";
+		getPrivateKeyHelper(expected1,seed);
+		
+	}
+
+	private void getPrivateKeyHelper(String expected, String seed) {
+		byte[] seedBytes = seed.getBytes();
+		byte[] output;
+		byte[] masterPrivateKey = Deterministic.bin_slowsha(seedBytes);
+		
+		output = Deterministic.getPrivateKey(masterPrivateKey,3);
+		String result = Deterministic.bytesToHex(output);
+		assertEquals(expected,result);
+		
 	}
 
 	public void slowshaHelper(String expected, String input) {
@@ -48,6 +67,8 @@ public class TestDeterministic {
 		assertEquals(expected,result);
 		
 	}
+	
+	
 	
 	
 
