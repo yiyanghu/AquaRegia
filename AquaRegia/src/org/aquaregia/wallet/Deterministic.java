b17d5bf1c82f9cb3b60e27fd9bda59551305169d
Yiyang Hu <yiyangh@gmail.com>
2014-04-17 00:25:20 -0500

    Add Deterministic getMasterKey and its junit test

--- AquaRegia/src/org/aquaregia/wallet/Deterministic.java
+++ AquaRegia/src/org/aquaregia/wallet/Deterministic.java
@@ -4,14 +4,20 @@ import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import javax.xml.bind.*;
+import com.google.bitcoin.core.ECKey;
 
 public class Deterministic {
 
-	public static byte[] bin_slowsha(byte[] input) throws NoSuchAlgorithmException, IOException {
+	public static byte[] bin_slowsha(byte[] input) {
 		byte[] result= null;
 		byte[] original = input;	
 		for(int i=0;i<100000;i++) {
-			MessageDigest md= MessageDigest.getInstance("SHA-256");
+			MessageDigest md = null;
+			try {
+				md = MessageDigest.getInstance("SHA-256");
+			} catch (NoSuchAlgorithmException e) {
+				return null;
+			}
 			result = new byte[original.length+ input.length];
 			System.arraycopy(input, 0, result, 0, input.length);
 			System.arraycopy(original,0,result,input.length,original.length);
@@ -26,6 +32,19 @@ public class Deterministic {
 		
 		return DatatypeConverter.printHexBinary(bytes).toLowerCase();
 			
+	}
+	
+	/**
+	 * This function generates master public key from a wallet seed
+	 * @param seed - a wallet seed
+	 * @return master public key
+	 */
+	public static byte[] getMasterPublicKey (byte[] seed) {
+		
+		byte[] masterPrivateKey= bin_slowsha(seed);
+		ECKey masterKey = new ECKey(masterPrivateKey,null);	
+		return masterKey.getPubKey(); 
+		
 	}
 	
 	
--- AquaRegia/test/org/aquaregia/wallet/TestDeterministic.java
+++ AquaRegia/test/org/aquaregia/wallet/TestDeterministic.java
@@ -11,29 +11,44 @@ public class TestDeterministic {
 
 	@Test
 	public void testBin_slowsha() {
-		String test1 = "This is something";	
+		String test1 = "This is something";
 		String expected1 = "b409b15310c82f3144ec7d3807ed407eda676e9bffe64d58450cd69f59280639";
-		slowshaHelper(expected1,test1);
-		
+		slowshaHelper(expected1, test1);
+
 		String test2 = "Aqua Regia is awesome!";
 		String expected2 = "fd72dcf2bcc215fb06cbf42f789c0b446e2f58ddfefdc2c5ed6426b62cc995c5";
-		slowshaHelper(expected2,test2);
-		
-		
+		slowshaHelper(expected2, test2);
+
 	}
 	
+	@Test
+	public void testGetMasterPublicKey() {
+		String test1 = "484ccb566edb66c65dd0fd2e4d90ef65";
+		String expected1 = "04484e42865b8e9a6ea8262fd1cde666b557393258ed598d842e563ad9e5e6c70a97e387eefdef123c1b8b4eb21fe210c6216ad7cc1e4186fbbba70f0e2c062c25";
+		getMasterPublicKeyHelper(expected1,test1);	
+	
+	}
+
 	public void slowshaHelper(String expected, String input) {
 		byte[] inputBytes = input.getBytes();
 		byte[] output;
-		try {
-			output = Deterministic.bin_slowsha(inputBytes);
-			String result= Deterministic.bytesToHex(output);
-			assertEquals(expected,result);
-		} catch (NoSuchAlgorithmException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			fail("This does not work for sha256");
-		}
+
+		output = Deterministic.bin_slowsha(inputBytes);
+		String result = Deterministic.bytesToHex(output);
+		assertEquals(expected, result);
+
 	}
+	
+	public void getMasterPublicKeyHelper(String expected, String input) {
+		byte[] inputBytes = input.getBytes();
+		byte[] output;
+		
+		output = Deterministic.getMasterPublicKey(inputBytes);
+		String result = Deterministic.bytesToHex(output);
+		assertEquals(expected,result);
+		
+	}
+	
+	
 
 }
