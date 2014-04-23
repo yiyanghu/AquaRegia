bd539babf31c8375c68a87c506b3325f648b0b11
Steve Halm <thereal1024@gmail.com>
2014-04-22 19:22:36 -0500

    fix menu not showing up

--- AquaRegia/src/org/aquaregia/ui/Menu.java
+++ AquaRegia/src/org/aquaregia/ui/Menu.java
@@ -11,7 +11,7 @@ public class Menu extends JMenuBar {
 	private final int ITEM_RADIO = 1;
 	private final int ITEM_CHECK = 2;
 
-	private JMenuBar menuBar;
+	//private JMenuBar menuBar;
 	private JMenu menuFile;
 	private JMenu menuWallet;
 	private JMenu menuHelp;
@@ -25,49 +25,43 @@ public class Menu extends JMenuBar {
 	private JMenuItem menuHelpWebsite;
 
 	public Menu() {
-
-
-
-		// create the menu bar
-		menuBar = new JMenuBar();
-
 		// create the file menu
 		menuFile = new JMenu("File");
 		menuFile.setMnemonic('F');
-		menuBar.add(menuFile);
+		this.add(menuFile);
 
 		// build a file menu items
-		menuFileNew = CreateMenuItem(menuFile, ITEM_PLAIN, "New", 'N');
-		menuFileOpen = CreateMenuItem(menuFile,ITEM_PLAIN,"Open...",'O');
-		menuFileQuit = CreateMenuItem(menuFile,ITEM_PLAIN,"Quit",'Q');
+		menuFileNew = createMenuItem(menuFile, ITEM_PLAIN, "New", 'N');
+		menuFileOpen = createMenuItem(menuFile,ITEM_PLAIN,"Open...",'O');
+		menuFileQuit = createMenuItem(menuFile,ITEM_PLAIN,"Quit",'Q');
 		menuFile.addSeparator();
 		
 		// create the wallet menu
 		menuWallet = new JMenu("Wallet");
 		menuWallet.setMnemonic('W');
-		menuBar.add(menuWallet);
+		this.add(menuWallet);
 		
 		// build the wallet menu options
-		menuWalletSeed = CreateMenuItem(menuWallet,ITEM_PLAIN,"Seed",'S');
-		menuWalletMPK = CreateMenuItem(menuWallet,ITEM_PLAIN,"Master Public Key",'M');
-		menuWalletExportHistory = CreateMenuItem(menuWallet,ITEM_PLAIN,"Export history",'E');
+		menuWalletSeed = createMenuItem(menuWallet,ITEM_PLAIN,"Seed",'S');
+		menuWalletMPK = createMenuItem(menuWallet,ITEM_PLAIN,"Master Public Key",'M');
+		menuWalletExportHistory = createMenuItem(menuWallet,ITEM_PLAIN,"Export history",'E');
 		menuFile.addSeparator();
 		
 		// create the help menu
 		menuHelp = new JMenu("Help");
 		menuHelp.setMnemonic('H');
-		menuBar.add(menuHelp);
+		this.add(menuHelp);
 		
 		// build the help menu options
-		menuHelpAbout = CreateMenuItem(menuHelp,ITEM_PLAIN,"About",'A');
-		menuHelpWebsite = CreateMenuItem(menuHelp,ITEM_PLAIN,"Website",'W');
+		menuHelpAbout = createMenuItem(menuHelp,ITEM_PLAIN,"About",'A');
+		menuHelpWebsite = createMenuItem(menuHelp,ITEM_PLAIN,"Website",'W');
 		menuHelp.addSeparator();
 		
 		
 	}
 
-	public JMenuItem CreateMenuItem(JMenu menu, int type, String text,
-			int acceleratorKey) {
+	public JMenuItem createMenuItem(JMenu menu, int type, String text,
+			int mnemonic) {
 
 		// create the item
 		JMenuItem menuItem;
@@ -90,8 +84,8 @@ public class Menu extends JMenuBar {
 		menuItem.setText(text);
 		
 		// add the accelerator key
-		if (acceleratorKey > 0)
-			menuItem.setMnemonic(acceleratorKey);
+		if (mnemonic > 0)
+			menuItem.setMnemonic(mnemonic);
 		
 		//menuItem.addActionListener((ActionListener) this);
 		
