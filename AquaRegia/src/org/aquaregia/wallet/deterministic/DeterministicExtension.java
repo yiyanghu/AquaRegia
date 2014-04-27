d8656a1be03a2358697c9de040cff12a3915d47a
Steve Halm <thereal1024@gmail.com>
2014-04-27 12:20:06 -0500

    progress on deterministic wallet

--- AquaRegia/src/org/aquaregia/wallet/deterministic/DeterministicExtension.java
+++ AquaRegia/src/org/aquaregia/wallet/deterministic/DeterministicExtension.java
@@ -1,8 +1,15 @@
 package org.aquaregia.wallet.deterministic;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.spongycastle.util.Arrays;
 
 import com.google.bitcoin.core.ECKey;
+import com.google.bitcoin.core.Transaction;
+import com.google.bitcoin.core.TransactionInput;
+import com.google.bitcoin.core.TransactionOutput;
 import com.google.bitcoin.core.Wallet;
 import com.google.bitcoin.core.WalletExtension;
 
@@ -12,7 +19,12 @@ public class DeterministicExtension implements WalletExtension {
 	private String seed;
 	private boolean isEncrypted;
 	private Wallet wallet;
+	/* Number of keys in wallet (next index to use) */
 	private int sequenceNum;
+	private boolean initialized = false;
+	
+	private byte[] masterPrivateKey;
+	private byte[] masterPublicKey;
 
 	@Override
 	public String getWalletExtensionID() {
@@ -52,9 +64,69 @@ public class DeterministicExtension implements WalletExtension {
 		byte[] cfg = Arrays.copyOfRange(data, 32, data.length);
 		isEncrypted = (cfg[0] & ENCRYPTED) != 0;
 		
+		generateMasterKeys();
+		
 		determineSequenceNumber();
+		initialized = true;
 	}
 	
+	public boolean isInitialized() {
+		return initialized;
+	}
+	
+	public void newSeedInit() {
+		seed = Deterministic.randomSeed();
+		// We assume that this is a new not encrypted wallet
+		// Besides, we can't encrypt without the password
+		sequenceNum = 0;
+		isEncrypted = false; 
+		initialized = true;
+		generateMasterKeys();
+		ensureFreeKeys();
+	}
+	
+	public void recoverSeedInit() {
+		// TODO
+	}
+	
+	
+	private void generateMasterKeys() {
+		masterPrivateKey = Deterministic.getMasterPrivateKey(seed.getBytes());
+		masterPublicKey = Deterministic.privateToPublic(masterPrivateKey);
+	}
+
+	private void ensureFreeKeys() {
+		// TODO ensure there are 5 unused keys in wallet
+		Map<ECKey, Boolean> used = new HashMap<ECKey, Boolean>();
+		
+		List<ECKey> allKeys = wallet.getKeys();
+		for (ECKey key : allKeys) {
+			used.put(key, false);
+		}
+		
+		List<Transaction> txs = wallet.getTransactionsByTime();
+		for (Transaction tx : txs) {
+			for (TransactionOutput txo : tx.getOutputs()) {
+				// if (txo.isWatched(wallet)) {
+				byte[] hash160 = txo.getScriptPubKey().getPubKeyHash();
+				ECKey loc = wallet.findKeyFromPubHash(hash160);
+				if (loc != null) {
+					Boolean b = used.put(loc, true);
+				}
+				//}
+			}
+			for (TransactionInput txi : tx.getInputs()) {
+				ECKey loc = wallet.findKeyFromPubKey(txi.getScriptSig().getPubKey());
+				if (loc != null) {
+					Boolean b = used.put(loc, true);
+				}
+			}
+		}
+		
+		// TODO
+		
+	}
+
 	private void determineSequenceNumber() {
 		// TODO determine next wallet key index
 		
@@ -63,6 +135,10 @@ public class DeterministicExtension implements WalletExtension {
 	public ECKey nextKey() {
 		// TODO get next key
 		return null;
+	}
+	
+	public String seedMnemonic() {
+		return Mnemonic.encode(seed);
 	}
 	
 	public void encrypt(CharSequence pw) {
